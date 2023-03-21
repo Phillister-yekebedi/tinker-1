15942827d18abee82f76917d4ccdaec8175bde4d
Phillister-yekebedi <124152221+Phillister-yekebedi@users.noreply.github.com>
2023-03-21 09:18:54 +0300

    Add files via upload

---
+++ measuring_distance1.ino
@@ -0,0 +1,30 @@
+// C++ code
+//
+/*
+  Code for measuring Distance
+*/
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println(0.01723 * readUltrasonicDistance(11, 10));
+  delay(500); // Wait for 500 millisecond(s)
+}
